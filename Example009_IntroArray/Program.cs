ef6628ae7156b901755a7a3529fa71663f119818
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-16 15:30:06 +0300

    Array

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+  int result=arg1;
+  if (arg2>result) result=arg2;
+  if (arg3>result) result=arg3;
+  return result;
+}
+
+int[] array = {11,21111,31,41,15,61,17,18,19};
+
+int result = Max (
+            Max(array[0], array[1], array[2]),
+            Max(array[3], array[4], array[5]),
+            Max(array[6], array[7], array[8])
+);
+
+Console.Write("Biggest number is: ");
+Console.WriteLine(result);
