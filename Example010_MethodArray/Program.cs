2c3275e678ca1eb9154b96581f7b75a77eb003ec
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-17 18:27:08 +0300

    Method and Array plus break

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int[]array = {12,20,39,19,56,65,73,81,19};
+int n=array.Length;
+int find = 19;
+int index = 0;
+while (index<n)
+{
+  if (array[index]==find)
+  {
+    Console.Write("Position: ");
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
