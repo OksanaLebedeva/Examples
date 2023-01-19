a7aad975cd63711e21790c6bf17f335b390df922
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-19 07:42:18 +0300

    Added for cycle

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,47 +1,59 @@
-﻿// Type 1
-void Method1()
-{
-  Console.WriteLine("The Author is ...");
-}
-//Method1();
+﻿// // Type 1
+// void Method1()
+// {
+//   Console.WriteLine("The Author is ...");
+// }
+// //Method1();
 
-// Type 2
-void Method2 (string msg)
-{
-  Console.WriteLine(msg);
-}
-//Method2("Text of the message");
+// // Type 2
+// void Method2 (string msg)
+// {
+//   Console.WriteLine(msg);
+// }
+// //Method2("Text of the message");
 
-void Method2_1(string msg, int count)
-{
-  int i = 0;
-  while (i<count)
-  {
-    Console.WriteLine(msg);
-    i++;
-  }
-}
-//Method2_1(count: 4, msg: "Text");
+// void Method2_1(string msg, int count)
+// {
+//   int i = 0;
+//   while (i<count)
+//   {
+//     Console.WriteLine(msg);
+//     i++;
+//   }
+// }
+// //Method2_1(count: 4, msg: "Text");
 
-// Type 3
-int Method3()
-{
-  return DateTime.Now.Year;
-}
-int year = Method3();
-//Console.WriteLine(year);
+// // Type 3
+// int Method3()
+// {
+//   return DateTime.Now.Year;
+// }
+// int year = Method3();
+// //Console.WriteLine(year);
 
-// Type 4
+// // Type 4
+// string Method4(int count, string text)
+// {
+//   int i = 0;
+//   string result = String.Empty;
+//   while (i < count) 
+//   {
+//     result = result + text;
+//     i++;
+//   }
+//   return result;
+// }
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-  int i = 0;
   string result = String.Empty;
-  while (i < count) 
+  for (int i = 0; i < count; i++)
   {
     result = result + text;
-    i++;
   }
   return result;
 }
-string res = Method4(10, "asdf");
+string res = Method4(10, "Sdf ");
 Console.WriteLine(res);
