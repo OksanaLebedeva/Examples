a24e01bb388ed4ffc4145605225439f68651f212
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-20 18:43:18 +0300

    Fibonacci

---
+++ Example018_Fibonacci/Program.cs
@@ -0,0 +1,9 @@
+﻿double Fibonacci(int n)
+{
+  if (n==1 || n==2) return 1;
+  else return Fibonacci(n-1)+Fibonacci(n-2); 
+}
+for (int i=1; i<50; i++)
+{
+  Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
