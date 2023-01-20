b8168cc6067d1c4fd25f4af2d535c9e21e1fb09b
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-20 18:35:02 +0300

    Factorial

---
+++ Example017_Factorial/Program.cs
@@ -0,0 +1,10 @@
+﻿double Factorial (int n)
+{
+  if(n==1) return 1;
+  else return n*Factorial(n-1);
+}
+// Console.WriteLine(Factorial(5));
+for (int i=1; i<40; i++)
+{
+  Console.WriteLine($"{i}! = {Factorial(i)}");
+}
